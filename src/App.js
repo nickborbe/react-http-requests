55377fec3a9a4118e5c4548a632f06342ff5b3e3
nickborbe <nickborbe@gmail.com>
2018-08-01 16:15:09 -0400

    adds axios call to click function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import './App.css'
+import axios from 'axios'
+
 class App extends Component {
-  constructor(){
-    super()
-    this.handleClick = this.handleClick.bind(this);
-  }
-  //
+  // constructor(){
+  //   super()
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+  // why do this whole .bind thing instead of just calling it differently in the view?
 
   handleClick(){
-    console.log("hello");
+    axios.get('https://api.github.com/users/nickborbe')
+    .then((res) => {
+      console.log(res)
+    });
+    
   }
 
 
   render () {
     return (
       <div className='button__container'>
-        <button className='button' onClick={this.handleClick}>
-        {/* why is it better to do .bind in the constructor instead of this thing below? */}
-        {/* <button className='button' onClick={()=>this.handleClick()}> */}
+        {/* <button className='button' onClick={this.handleClick}> */}
+        {/* this is instead of .bind in the constructor - it's a more recent fix, .bind is old syntax */}
+        <button className='button' onClick={()=>this.handleClick()}>
         Click Me
         </button>
       </div>
