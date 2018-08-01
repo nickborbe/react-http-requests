fd03f2d2f1b6a7b3fb1b4287d4b9960956ee7e1e
nickborbe <nickborbe@gmail.com>
2018-08-01 16:35:46 -0400

    adds monstrocity of code to track the value of the input box

--- src/App.js
+++ src/App.js
@@ -6,23 +6,40 @@ class App extends Component {
   constructor(props){
     super(props)
     // this.handleClick = this.handleClick.bind(this);
-    this.state = {username: null}
+    this.state = {
+      username: null,
+      urlToSend: '',
+    }
   }
   // why do this whole .bind thing instead of just calling it differently in the view?
+
+  theURL = ''
+  
 
   handleClick(){
     axios.get('https://api.github.com/users/nickborbe')
     .then((res) => {
       console.log(res.data)
+      console.log(this.theURL)
       this.setState({username: res.data.name})
-    });
-    
+    }); 
+  }
+
+  updateUrlToSend(e){
+    this.setState({
+      username: this.state.username,
+      urlToSend: e.target.value,
+    })
+
   }
 
 
   render () {
     return (
       <div className='button__container'>
+      <input value ={this.state.urlToSend} onChange={e => this.updateUrlToSend(e)}>
+      </input>
+      <br></br> <br></br>  <br></br>
         {/* <button className='button' onClick={this.handleClick}> */}
         {/* this is instead of .bind in the constructor - it's a more recent fix, .bind is old syntax */}
         <button className='button' onClick={()=>this.handleClick()}>
