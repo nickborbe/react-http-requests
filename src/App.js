a78f26b4ce77411b8c426e3537bc72651f07ec1d
nickborbe <nickborbe@gmail.com>
2018-08-01 13:53:15 -0400

    creates the click function to handle the click event, basic say hello function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 class App extends Component {
+  constructor(){
+    super()
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    console.log("hello");
+  }
+
+
   render () {
     return (
       <div className='button__container'>
